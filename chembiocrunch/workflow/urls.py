272b9d99fe32a054f31822555d98af73253f211c
Paul Barrett <paul.barrett@sgc.ox.ac.uk>
2014-07-24 14:09:59 +0100

    Adding success page for creating workflow

--- chembiocrunch/workflow/urls.py
+++ chembiocrunch/workflow/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 from .views import calculations, visualise, workflows
 
 urlpatterns = [
     url(r'^$', workflows.WorkflowListView.as_view() , name='workflow_list'),
     url(r'^create/$', workflows.WorkflowCreateView.as_view(), name='workflow_create'),
+    url(r'^create/success/$', TemplateView.as_view(template_name="workflows/workflow_create_success.html"), name='workflow_create_success'),
 
 ]
