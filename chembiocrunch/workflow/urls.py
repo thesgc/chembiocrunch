4120dce9cf9a897d973eb76e0b2afce51e09d84b
Strets123 <strets123@gmail.com>
2014-07-31 18:12:13 +0100

    adding visualisations and making them save

--- chembiocrunch/workflow/urls.py
+++ chembiocrunch/workflow/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/visualisation_view$', workflows.VisualisationView.as_view(), name='visualisation_view'),
     url(r'^create/success/$', TemplateView.as_view(template_name="workflows/workflow_create_success.html"), name='workflow_create_success'),
    # url(r'^(?P<pk>[0-9]+)/visualisation_export/$', workflows.VisualisationExportView.as_view(), name='visualisation_export'),
-    url(r'^(?P<pk>[0-9]+)/visualisation_builder/(?P<workflow_revision_id>[0-9]+)/$', workflows.VisualisationBuilderView.as_view(), name='visualisation_builder')
+    url(r'^(?P<pk>[0-9]+)/visualisation_builder/(?P<workflow_revision_id>[0-9]+)/$', workflows.VisualisationBuilderView.as_view(), name='visualisation_builder'),
+    url(r'^(?P<pk>[0-9]+)/visualisation_builder/(?P<workflow_revision_id>[0-9]+)/(?P<visualisation_id>[0-9]+)/$', workflows.VisualisationUpdateView.as_view(), name='visualisation_update_view')
+
 ]
