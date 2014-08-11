cd05a8ab89c6474f75de2dfca7e6f25172802799
Strets123 <strets123@gmail.com>
2014-08-11 16:53:12 +0100

    adding webauth link

--- chembiocrunch/workflow/urls.py
+++ chembiocrunch/workflow/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     url(r'^create-ic50/(?P<form_type>[a-zA-Z0-9]+)$', workflows.Ic50WorkflowCreateView.as_view(), name='workflow_ic50_create'),
     url(r'^(?P<pk>[0-9]+)/workflow_data_mappings_edit/$', workflows.WorkflowDataMappingEditView.as_view(), name='workflow_data_mappings_edit'),
     url(r'^(?P<pk>[0-9]+)/heatmap/$', workflows.WorkflowHeatmapView.as_view(), name='workflow_ic50_heatmap'),
-    url(r'^(?P<pk>[0-9]+)/visualisation_view$', workflows.VisualisationView.as_view(), name='visualisation_view'),
+    url(r'^(?P<pk>[0-9]+)/visualisation_view/(?P<format>[a-zA-Z0-9]+)/$', workflows.VisualisationView.as_view(), name='visualisation_view'),
     url(r'^create/success/$', TemplateView.as_view(template_name="workflows/workflow_create_success.html"), name='workflow_create_success'),
    # url(r'^(?P<pk>[0-9]+)/visualisation_export/$', workflows.VisualisationExportView.as_view(), name='visualisation_export'),
     url(r'^(?P<pk>[0-9]+)/visualisation_builder/(?P<workflow_revision_id>[0-9]+)/$', workflows.VisualisationBuilderView.as_view(), name='visualisation_builder'),
