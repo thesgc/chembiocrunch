7b987d89283629d94c88449d414f9bc82c2be5b5
paulbarrett79 <paulbarrett79@gmail.com>
2014-09-15 16:18:43 +0100

    unwanted artifact

--- chembiocrunch/ic50/views.py
+++ chembiocrunch/ic50/views.py
@@ -377,7 +377,7 @@ class Ic50VisualisationView(VisualisationView):
         self.object.html = curve_fitter.svg
         self.object.results = json.dumps({"values": curve_fitter.results})
 
-        self.object.save()source miniconda/bin/activate cbc
+        self.object.save()
 
     def get_html(self):
         err = self.object.error_class()
