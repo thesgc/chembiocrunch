5b5f10d3229e4cb784d1c150747a42c8083efbde
Strets123 <strets123@gmail.com>
2014-11-04 16:52:28 +0000

    models

--- chembiocrunch/workflow/models.py
+++ chembiocrunch/workflow/models.py
@@ -53,7 +53,7 @@ class WorkflowManager(models.Manager):
     def get_user_records(self, user):
         groups_list = user.groups.filter(name__in =["affiliation:sgc", "affiliation:tdi"])
         if  groups_list.count > 0:
-            return self.filter(created_by__groups__id__in=all_users_in_group.values_list(["id"], flat=True))
+            return self.filter(created_by__groups__id__in=groups_list.values_list(["id"], flat=True))
         else:
             return self.filter(created_by__id=user.id)
 
