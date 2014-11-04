d10399e8db3f99e10bcaa9fef7d10c85752c7dcc
Strets123 <strets123@gmail.com>
2014-11-04 16:33:53 +0000

    models

--- chembiocrunch/ic50/models.py
+++ chembiocrunch/ic50/models.py
@@ -43,8 +43,11 @@ from cbc_common import dataframe_handler
 
 class IC50WorkflowManager(models.Manager):
     def get_user_records(self, user):
-        if user.groups.filter(name__in =["affiliation:sgc", "affiliation:tdi"]):
-            return self.filter(created_by__group__name__in=["affiliation:sgc", "affiliation:tdi"])
+        groups_list = user.groups.filter(name__in =["affiliation:sgc", "affiliation:tdi"])
+        if  groups_list.count > 0:
+            all_users_in_group = groups_list.select_related('User').all()
+            
+            return self.filter(created_by__in=all_users_in_group)
         else:
             return self.filter(created_by__id=user.id)
 
